1ac5e5a7c1d6dbfe0b607033c9999c9c79c8ceb6
root <1033885715@qq.com>
2023-04-21 16:28:09 +0800

    add  RandomLoadBalancer settings

--- eurekaClientConsumer/src/main/java/cc/huerpu/eurekaclient/loadbalancer/CustomLoadBalancerConfiguration.java
+++ eurekaClientConsumer/src/main/java/cc/huerpu/eurekaclient/loadbalancer/CustomLoadBalancerConfiguration.java
@@ -3,7 +3,6 @@ package cc.huerpu.eurekaclient.loadbalancer;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.loadbalancer.core.RandomLoadBalancer;
 import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
-import org.springframework.cloud.loadbalancer.core.RoundRobinLoadBalancer;
 import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
 import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.context.annotation.Bean;
