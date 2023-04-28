4a64196e633e10b13a16dc928df717bb8920f688
jing <1033885715@qq.com>
2023-04-28 17:33:22 +0800

    add modify eurekaserver auth settings

--- eurekaServer/src/main/java/cc/huerpu/eurekaserver/security/WebSecurityConfig.java
+++ eurekaServer/src/main/java/cc/huerpu/eurekaserver/security/WebSecurityConfig.java
@@ -1,22 +1,34 @@
 package cc.huerpu.eurekaserver.security;
 
 
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@AutoConfigureAfter(WebSecurityConfig.class)
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
 public class WebSecurityConfig
 {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http.csrf().disable();
-        http.authorizeHttpRequests().anyRequest().authenticated().and().httpBasic();
+        http.authorizeHttpRequests()
+                .requestMatchers("/eureka/**")
+                .permitAll()
+                .requestMatchers("/actuator/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+
         return http.build();
     }
 }
+
+
+
+
