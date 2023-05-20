37fe1a965a2d2fc75fe38ca4461c5504cef69c4b
jing <1033885715@qq.com>
2023-05-20 11:42:04 +0800

    add thread sleep 1s

--- eurekaClient/src/main/java/cc/huerpu/eurekaclient/controller/UserController.java
+++ eurekaClient/src/main/java/cc/huerpu/eurekaclient/controller/UserController.java
@@ -7,9 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @RequestMapping("/getUserById")
-    public String getUserById(){
-
-        return "{id:1,name:jason,age:23}";
+    public String getUserById() throws Exception{
+        Thread.sleep(10000);
+        return "{id:1,name:jason,age:23,sex: man}";
     }
-
 }
